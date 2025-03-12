9a5c8bd123dbf7f545522cd3b81e6e834b25e059
Jon Hogendorp <64735813+jonhogendorp@users.noreply.github.com>
2025-03-12 23:16:52 +0100

    update CSS imports and add text size variable; implement employer logo component

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,2 +1,2 @@
-<a class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-white text-xs transition-colors duration-300"
+<a class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-white text-2xs transition-colors duration-300"
     href="#">{{ $slot }}</a>
