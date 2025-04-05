c03ee41097564d74918f6f1328df87c3d22b891a
Jon Hogendorp <64735813+jonhogendorp@users.noreply.github.com>
2025-04-05 23:56:59 +0200

    add Tag model, factory, and migration; update Job model to include employer relationship

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model;     
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Job extends Model
@@ -13,14 +13,28 @@ class Job extends Model
     use HasFactory;
 
 
-
-    public function user(): BelongsTo
+    public function tag()
     {
-        return $this->belongsTo(User::class);
+
     }
 
-    public function jobs(): HasMany
+    public function tags()
     {
-        return $this->hasMany(Job::class);
+
     }
+
+    public function employer(): BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
+
+    // public function user(): BelongsTo
+    // {
+    //     return $this->belongsTo(User::class);
+    // }
+
+    // public function jobs(): HasMany
+    // {
+    //     return $this->hasMany(Job::class);
+    // }
 }
