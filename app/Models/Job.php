0521b617a613b93811a667de6729ac083d77f53b
Jon Hogendorp <64735813+jonhogendorp@users.noreply.github.com>
2025-04-12 17:21:55 +0200

    add job-tag pivot table migration; update Job model to manage tags

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Model;     
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Job extends Model
@@ -13,14 +14,17 @@ class Job extends Model
     use HasFactory;
 
 
-    public function tag()
+    public function tag(string $tag): void
     {
+        $tag = Tag::firstOrCreate(['name' => $tag]);
 
+        $this->tags()->attach($tag);
     }
 
-    public function tags()
-    {
 
+    public function tags():BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
     }
 
     public function employer(): BelongsTo
